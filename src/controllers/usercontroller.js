fdef66fe8f5bb6e79cb3c69c492a427537e2c828
vijeta33 <vijetahiwarkar@gmail.com>
2021-11-26 19:59:59 +0530

    assingnmentcrypto

--- src/controllers/usercontroller.js
+++ src/controllers/usercontroller.js
@@ -1,54 +1,36 @@
-const userModel = require("../models/userModel.js")
+
 const jwt = require('jsonwebtoken')
 const axios = require("axios")
+const coinsModel = require('../models/coinsModel')
 
 
 
 
-const getweatherofLondon = async function (req, res){
+const getcoins = async function (req, res){
 
     try{ 
-        let q =req.query.q
-        console.log(q)
-        let appid = req.query.appid
-        console.log(appid)
 
-
+        
+        
         let options = {
           method : "get",
-          url : `http://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${appid}`
+          url : ("http://api.coincap.io/v2/assets")
         }
+
         let response= await axios(options)
-        console.log(response)
-        let weather = response.data
-        console.log(weather)
-        res.status(200).send( {msg: "Success", data: weather} )
 
-    }
-    catch(err) {
-        console.log(err.message)
-        res.status(500).send( { msg: "Something went wrong" } )
-    }
-}
-
-const Londontemp = async function (req, res){
-
-    try{ 
-    
-        let cities  =  ["Bengaluru","Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"]
-        let cityObjArray = [];
-        let length = cities.length
-        for(let i=0; i<length; i++){
-            let obj = {city: cities[i]}
-            let resp = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=d03c6511ac589c04857f3a2bc06681fd`)
-            console.log(resp.data.main.temp)
-            obj.temp= resp.data.main.temp
-
-            cityObjArray.push(obj);
+        let dataArray = response.data.data
+        for (i in dataArray) {
+            let coinsData = {
+                symbol: dataArray[i].symbol,
+                name: dataArray[i].name,
+                marketCapUsd: dataArray[i].marketCapUsd,
+                priceUsd: dataArray[i].priceUsd
+            }
+            await coinsModel.create(coinsData)
         }
-        let sorted = cityObjArray.sort(  function(a, b) { return a.temp - b.temp } )
-        console.log(sorted)
-        res.status(200).send( {status: true, data: sorted} )
+        dataArray.sort(function (a,b){ return (a.changePercent24Hr)- (b.changePercent24Hr)})
+        res.status(200).send( {msg: "Success", data: dataArray} )
 
     }
     catch(err) {
@@ -56,33 +38,8 @@ const Londontemp = async function (req, res){
         res.status(500).send( { msg: "Something went wrong" } )
     }
 }
-        
-    
-        // const getOtp = async function (req, res){
-
-        //     try{ 
-        
-        //          let options = {
-        //           method : "post", // method has to be post
-        //           url : `https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP`,
-        //           data: { "mobile": req.body.mobile  } // we are sending the json body in the data 
-        //         }
-        //         let response= await axios(options)
-        
-        //         let id= response.data
-        //         res.status(200).send( {msg: "Success", data: id} )
-        
-        //     }
-        //     catch(err) {
-        //         console.log(err.message)
-        //         res.status(500).send( { msg: "Something went wrong" } )
-        //     }
-        // }
-        
 
 
-//module.exports.getOtp = getOtp
-module.exports.getweatherofLondon = getweatherofLondon
-module.exports.Londontemp = Londontemp
+module.exports.getcoins = getcoins
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,17 +2,16 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken')
 const axios = require("axios")
+const coinsModel = require("../models/coinsModel")
 
 const userController =require("../controllers/usercontroller")
-//const commonMw = require("../middleware/commonmw")
 
 
 
 
 
-//router.post("/cowin/getOtp", userController.getOtp)
-router.get("/getweather", userController.getweatherofLondon)
-router.get("/londontemp", userController.Londontemp)
+
+router.get("/assets" , userController.getcoins)
 
 
 
